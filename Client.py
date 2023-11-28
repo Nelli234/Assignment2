d43a9a32c54108cc8cbf105b3fede295a9825bb5
Nelli234 <145477248+Nelli234@users.noreply.github.com>
2023-11-28 14:02:15 -0600

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,15 +1,20 @@
-# runs on Pi
-
+#on PC
+#Nelson Hucklebridge
+#tprg 2131-01
+#2023-11-28
+#This program recieves JSON objects from a raspberry pi and prints it on pc
 import socket
 from time import sleep as sl
 s = socket.socket()
 host = '10.102.13.79'# ip of rapberry pi, running the server
 port = 5000
-s.connect((host, port))
 
-
-print(s.recv(1024).decode('utf-8'))
-s.close()
-
-
-print("All done")
+if __name__ == "__main__":#Main gaurd to exit gracefully 
+    try:
+        s.connect((host, port))
+        print(s.recv(1024).decode('utf-8'))#decodes byte
+        s.close()#exits
+        print("All done")
+    except KeyboardInterrupt:#use CTRL-C
+        print("exiting")
+    
